18c09cf76d4c898d3aa03d88873213dbc32f23d7
Half-Shot <will@half-shot.uk>
2024-12-09 09:50:20 +0000

    add a header

--- src/soundUtils.ts
+++ src/soundUtils.ts
@@ -1,3 +1,10 @@
+/*
+Copyright 2024 New Vector Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only
+Please see LICENSE in the repository root for full details.
+*/
+
 import { logger } from "matrix-js-sdk/src/logger";
 
 type SoundDefinition = { mp3?: string; ogg: string };
