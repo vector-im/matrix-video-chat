cd0568b1284bb36b188f023db8805d385dc238b5
Hugh Nimmo-Smith <hughns@element.io>
2024-12-11 09:35:41 +0000

    Lint

--- src/settings/DeveloperSettingsTab.tsx
+++ src/settings/DeveloperSettingsTab.tsx
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 Please see LICENSE in the repository root for full details.
 */
 
-import { ChangeEvent, FC, useCallback } from "react";
+import { type ChangeEvent, type FC, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
 import { FieldRow, InputField } from "../input/Input";
